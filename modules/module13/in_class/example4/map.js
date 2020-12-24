fdcbf51432021cec3e125ef0934de11ae3d3228e
Jon Richards <jrseti@gmail.com>
2020-12-23 19:25:19 -0800

    Added

--- modules/module13/in_class/example4/map.js
+++ modules/module13/in_class/example4/map.js
@@ -1,9 +1,11 @@
 var myMap = L.map("map", {
     center: [41.1400, -104.8202],
     zoom: 4
-  });
+#    center: [61.2686, -149.4815],
+    #    zoom: 9
+    });
 
-  L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     id: 'mapbox/outdoors-v11',
@@ -37,6 +39,21 @@ var cities = [{
 }
 ];
 
-cities.forEach(city=> L.marker(city.location)
+cities.forEach(city => L.marker(city.location)
     .bindPopup(`<h1>${city.name}</h1> <b></b> <h3>${city.city_info}</h3> <p>Hello</p>`)
+    .addTo(myMap));
+var stores = [{
+    location: [61.130249, -149.883896],
+    name: "100th & C St - Anchorage",
+    store_type: "Company Owned"
+},
+{
+    location: [61.145080, -149.862800],
+    name: "Old Seward & Diamond",
+    store_type: "Company Owned"
+}
+];
+
+stores.forEach(store => L.marker(store.location)
+    .bindPopup(`<h1>${store.name}</h1> <b></b> <h3>${store.store_type}</h3> <p>Hello</p>`)
     .addTo(myMap));
